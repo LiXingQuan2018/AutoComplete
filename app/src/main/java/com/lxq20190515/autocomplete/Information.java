b03101b25a32d3f0db921893d1a3e5c8875892a1
Admin <2416677216@qq.com>
2019-06-21 12:08:51 +0800

    android Internet

---
+++ app/src/main/java/com/lxq20190515/autocomplete/Information.java
@@ -0,0 +1,26 @@
+package com.lxq20190515.autocomplete;
+
+public class Information {
+    private String answer;
+    private String name;
+    /*private int step;
+    private String time;
+    private int heat;
+    private float km;*/
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
